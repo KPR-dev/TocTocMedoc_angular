2c9560be62786d5b05e6119be095bd01f0192dda
Orphee NVE HOUNKPONOU <orpheenve@hotmail.com>
2022-11-24 14:24:11 +0100

    feat save

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiURL: "https://epg-supervisor-api.pivot40.tech",
   apiKey: "dbab1b45-f454-4568-9fcc-47692b8e6319",
-  pageItemCount: 250,
+  pageItemCount: 1000,
   pharmacies: [
     "623a45be1b6f66b46e99a03d",
     "6237594a2fb38ca16715489f",
